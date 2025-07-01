b7562603832510916a6f3d1a0f4e770edab44dce
jagadeshchilla <chillajagadesh68@gmail.com>
2025-07-01 16:56:41 +0530

    Fix Python version compatibility issues

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 from typing import List
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
@@ -18,7 +18,8 @@ version = "0.0.1",
 author = "jagadeesh",
 author_email = "jagadeesh@gmail.com",
 packages = find_packages(),
-install_requires = get_requirements('requirements.txt')
+install_requires = get_requirements('requirements.txt'),
+python_requires=">=3.8"
 )
 
 
